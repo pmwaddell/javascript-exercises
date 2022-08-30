3f7195d918aa3d29f2a4ce4cbd754f572f1c3002
Peter Waddell <pmwaddell9@gmail.com>
2022-08-29 21:53:23 -0500

    First 4 solutions

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(s, timesToRepeat) {
+    result = "";
+    if (timesToRepeat < 0) {
+        return 'ERROR';
+    }
 
+    for (let i = 0; i < timesToRepeat; i++) {
+        result = result + s;
+    }
+    return result;
 };
 
 // Do not edit below this line
